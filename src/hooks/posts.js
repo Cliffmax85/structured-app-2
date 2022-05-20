ca369392c6176a2c651259fe8b8f5f6df79ae66c
cliffmax85 <cliffmax85@gmail.com>
2022-05-20 11:40:34 -0700

    wrote posts hooks for create, update and get by id

--- src/hooks/posts.js
+++ src/hooks/posts.js
@@ -1,2 +1,77 @@
 import { useContext, useEffect, useState } from "react";
 import { PostsContext } from "../context/PostsContext";
+import { getPost, getPosts, updatePost, createPost } from "../services/posts";
+
+export function usePosts() {
+    const context = useContext(PostsContext);
+
+    if (context === undefined) {
+        throw new Error('usePosts must be wrapped within a PostsContext');
+    }
+
+    const { posts, dispatch } = context;
+
+    
+    useEffect(() => {
+        if (posts) return;
+        const load = async () => {
+            try {
+                const payload = await getPosts();
+                dispatch({ type: 'reset', payload: payload });
+            } catch (error) {
+                throw new Error('Somethign went wrong getting data')
+            }
+        };
+        load();
+    }, []);
+
+    const addNewPost = async (post) => {
+        try {
+            const newPost = await createPost(post);
+            dispatch({ type: 'create', payload: newPost })
+        } catch (error) {
+            throw new Error('could not add new post');
+        }
+    };
+    return { posts, addNewPost }
+}
+
+export function usePost(id) {
+    const context = useContext(PostsContext);
+
+    if (context === undefined) {
+        throw new Error('usePost must be wrapped within a PostsContext');
+    }
+
+    const { posts, dispatch } = context;
+
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        const load = async () => {
+            try {
+                const post = await getPost(id);
+                setPost(post);
+            } catch (error) {
+                throw new Error('Falied to get post by id')
+            }
+        };
+          load();
+    }, [id]);
+
+    const update = async (edits) => {
+        if (!post) return;
+
+        try {
+            const updated = await updatePost({ ...post, ...edits });
+            const payload = { ...updated, name: posts.name };
+            setPost(payload);
+            if (posts) dispatch({ type: 'update', payload });
+            return payload;
+        } catch (error) {
+            throw new Error('failed to update');
+        }
+    }
+
+    return { post, update };
+}
