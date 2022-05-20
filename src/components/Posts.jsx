7f3297c8424918e5bc1ceb6c126f1f3f0f34a2cf
cliffmax85 <cliffmax85@gmail.com>
2022-05-20 13:09:19 -0700

    changed post form to use hhoks, doesnt look like its hitting supabase now

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,43 +1,83 @@
 import { useState } from 'react';
 import { createPost } from '../services/posts';
 import { useUser } from '../context/UserContext';
+import { useForm } from '../hooks/useForm';
+import { usePosts } from '../hooks/posts';
+
 
 export default function PostForm({ addPost }) {
-  const { user } = useUser();
-  const [description, setDescription] = useState('');
-  const [name, setName] = useState('')
+  const { addNewPost } = usePosts();
+  const { formState, handleFormChange, setFormError } = useForm({ 
+    name: '',
+    description: '',
+  });
 
-  const postInput = async (e) => {
+  async function handleSubmit(e) {
     e.preventDefault();
-    const entry = await createPost({ user_id: user.id, description, email: user.email, name });
-    addPost(entry);
-    setDescription('');
-    setName('');
-  };
+
+    if (!formState.name) return setFormError('gimme a name!');
+    if (!formState.description) return setFormError('gimme your post');
+    addNewPost(formState);
+
+
+  }
 
   return (
-    <>
-      <div>
-        <form onSubmit={postInput}>
-          <textarea
-            name="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder='enter post!'
-          />
-          <textarea
-            name='name'
-            value={name}
-            placeholder='enter name'
-            onChange={(e) => setName(e.target.value)}
-            />
-          <button 
-            type="submit"
-          >
-              Add a post
-          </button>
-        </form>
-      </div>
-    </>
-  );
+    <section>
+      <p>Post your post</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          id='name'
+          name='name'
+          placeholder='name'
+          onChange={handleFormChange}
+        />
+        <input
+        id='description'
+        name='description'
+        placeholder='description'
+        onChange={handleFormChange}
+        />
+        <button>Add post</button>
+      </form>
+    </section>
+  )
+
+  // const { user } = useUser();
+  // const [description, setDescription] = useState('');
+  // const [name, setName] = useState('')
+
+  // const postInput = async (e) => {
+  //   e.preventDefault();
+  //   const entry = await createPost({ user_id: user.id, description, email: user.email, name });
+  //   addPost(entry);
+  //   setDescription('');
+  //   setName('');
+  // };
+
+  // return (
+  //   <>
+  //     <div>
+  //       <form onSubmit={postInput}>
+  //         <textarea
+  //           name="description"
+  //           value={description}
+  //           onChange={(e) => setDescription(e.target.value)}
+  //           placeholder='enter post!'
+  //         />
+  //         <textarea
+  //           name='name'
+  //           value={name}
+  //           placeholder='enter name'
+  //           onChange={(e) => setName(e.target.value)}
+  //           />
+  //         <button 
+  //           type="submit"
+  //         >
+  //             Add a post
+  //         </button>
+  //       </form>
+  //     </div>
+  //   </>
+  // );
 }
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export function useForm(inputs = {}) {
-  const [formState, setFormState] = useState(inputs);
+  const [formState, setFormState] = useState({ ...inputs });
   const [formError, setFormError] = useState('');
 
   const handleFormChange = (event) => {
